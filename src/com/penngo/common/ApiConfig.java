3980e482c2b14a2eac317b30c8262f882933d691
penngo@163.com <my365894643>
2015-07-19 23:57:44 +0800

    no message

--- src/com/penngo/common/ApiConfig.java
+++ src/com/penngo/common/ApiConfig.java
@@ -68,10 +68,7 @@ public class ApiConfig extends JFinalConfig {
 		
 	}
 	
-	/**
-	 * 建议使用 JFinal 手册推荐的方式启动项目
-	 * 运行此 main 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此
-	 */
+
 	public static void main(String[] args) {
 		//System.out.println("args=====" + args.length);
 		int port = 8000; // 默认端口
--- startup.bat
+++ startup.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar run.jar
+java -jar run.jar 8000
